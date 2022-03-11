927c0ba49af1caf2d6b4b2cd53b89528ad1d7f4f
Kolejojoses <kolejojoses@gmail.com>
2022-03-11 12:57:32 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 0-9
+ * 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numbe;
+	
+	for (numbe = 0; numbe <= 9; numbe++)
+	{
+		putchar(numbe % 10 + '0');
+		if (numbe < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
