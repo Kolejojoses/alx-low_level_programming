9486e3866a2d5b7ff7f9677cec10cf0fd31c9ede
Kolejojoses <kolejojoses@gmail.com>
2022-03-11 14:25:02 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - combination
+ *
+ * Return - 0 (Success)
+ */
+int main(void)
+{
+	int tens, unit;
+
+	for (unit = 0; unit <= 9; unit++)
+	{
+		for (tens = 0; tens <= 9; tens++)
+		{
+			if (tens < unit)
+			{
+				putchar(tens % 10 + '0');
+				putchar(unit % 10 + '0');
+				if (tens != 8 && unit != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
