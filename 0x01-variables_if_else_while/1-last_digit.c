17b9a6cca3dc272ec97189870867a76316797fb9
Kolejojoses <kolejojoses@gmail.com>
2022-03-11 07:46:05 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("%d and is greater than 5\n", n);
 	else if (n == 0)
 		printf("%d and is 0\n", n);
-	else if (0 < n < 6)
+	else if (n > 0 && n < 6)
 		printf("%d is less than 6 and not 0\n", n);
 	return (0);
 }
