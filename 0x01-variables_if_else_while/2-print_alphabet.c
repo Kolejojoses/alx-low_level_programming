884e9206718bd06592190f722cb3071192a8f7a6
Kolejojoses <kolejojoses@gmail.com>
2022-03-11 08:40:16 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	alphabet = "a";
+	while (alphabet < "z")
+		putchar("%c", alphabet);
+		alphabet ++;
+	return (0);
+}
