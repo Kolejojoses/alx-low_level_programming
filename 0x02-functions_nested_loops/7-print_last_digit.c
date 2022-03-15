841d1806c7055ff8baaea3440b57789c9ce1bb9c
Kolejojoses <kolejojoses@gmail.com>
2022-03-15 16:50:17 +0000

    7' _1 stop d

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+*print_last_digit - a function that prints the last digit of a number
+*@b: notation
+*Return: last digit
+*/
+
+int print_last_digit(int b)
+{
+	return (a % 10 + '0');
+}
