f85bee8484ab833d4fc92ca9bfc5c33698ce141e
Kolejojoses <kolejojoses@gmail.com>
2022-03-15 13:08:33 +0000

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+*main - a function that prints the alphabet
+*
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void);
+{
+	char alphabet;
+
+	for (alphabet = a; alphabet <= z; alphabet++)
+		_putchar(alphabet);
+		if (alphabet != z)
+			_putchar(' ');
+}
