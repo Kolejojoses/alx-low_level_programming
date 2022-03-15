c6d2938f698f25704d3877bf9ee83f72827271fc
Kolejojoses <kolejojoses@gmail.com>
2022-03-15 21:06:45 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,15 +16,13 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d", n);
-				_putchar(',');
+				printf("%d, ", n);
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 			}
 		}
-		_putchar('\n');
 	}
 	else
 	{
@@ -32,14 +30,12 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d", n);
-				_putchar(',');
+				printf("%d, ", n);
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 			}
 		}
-		_putchar('\n');
 	}
 }
