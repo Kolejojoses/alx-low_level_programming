481727e81edcb5960b37b1d61556e07780caf9f3
Kolejojoses <kolejojoses@gmail.com>
2022-03-15 14:45:11 +0000

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,8 +13,8 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int n;
 
-	for (n = 0; n <= 10; n++)
-	{	
+	for (n = 0; n <= 9; n++)
+	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
