d856475d32779780391f56f80cb6938347a01d9e
Kolejojoses <kolejojoses@gmail.com>
2022-03-16 10:42:06 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 						_putchar((z / 10) % 10 + '0');
 						_putchar(z % 10 + '0');
 					}
-					else if (z >= 100 && z < 1000)
+					else if (z >= 100)
 					{
 						_putchar((z / 100) % 10 + '0');
 						_putchar((z / 10) % 10 + '0');
