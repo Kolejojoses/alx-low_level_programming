f85148982514d4cfba2f2fee918274a6847dc7a8
Kolejojoses <kolejojoses@gmail.com>
2022-03-16 19:10:15 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,28 +17,23 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
-				if (y != 0)
+				if (z < 10)
 				{
-					_putchar(',');
 					_putchar(' ');
-					if (z < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(z % 10 + '0');
-					}
-					else if (z >= 10 && z < 100)
-					{
-						_putchar(' ');
-						_putchar((z / 10) % 10 + '0');
-						_putchar(z % 10 + '0');
-					}
-					else if (z >= 100)
-					{
-						_putchar((z / 100) % 10 + '0');
-						_putchar((z / 10) % 10 + '0');
-						_putchar(z % 10 + '0');
-					}
+					_putchar(' ');
+					_putchar(z % 10 + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(' ');
+					_putchar((z / 10) % 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar((z / 100) % 10 + '0');
+					_putchar((z / 10) % 10 + '0');
+					_putchar(z % 10 + '0');
 				}
 				else
 				{
