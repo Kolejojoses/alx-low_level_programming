adef3f325d62ed6c894d91d9089234de082c1e67
Kolejojoses <kolejojoses@gmail.com>
2022-03-16 19:36:17 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,29 +17,28 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
-				if (z < 10 && y != 0)
+				if (y != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z % 10 + '0');
-				}
-				else if (z >= 10 && z < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((z / 10) % 10 + '0');
-					_putchar(z % 10 + '0');
-				}
-				else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) % 10 + '0');
-					_putchar((z / 10) % 10 + '0');
-					_putchar(z % 10 + '0');
+					if (z < 10 && y != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(z % 10 + '0');
+					}
+					else if (z >= 10 && z < 100)
+					{
+						_putchar(' ');
+						_putchar((z / 10) % 10 + '0');
+						_putchar(z % 10 + '0');
+					}
+					else if (z >= 100)
+					{
+						_putchar((z / 100) % 10 + '0');
+						_putchar((z / 10) % 10 + '0');
+						_putchar(z % 10 + '0');
+					}
 				}
 				else if (y == 0)
 				{
