297612f25029f9d389c7e00a98e13e12b42eb58d
Kolejojoses <kolejojoses@gmail.com>
2022-03-17 19:50:09 +0000

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
 * print_line -  draws a straight line in the terminal
 * @n: Notation
@@ -13,9 +14,11 @@ void print_line(int n)
 	if (n <= 0)
 		_putchar('\n');
 	else
+	{	
 		for (a = 1; a <= n; a++)
 		{
 			_putchar('_');
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
