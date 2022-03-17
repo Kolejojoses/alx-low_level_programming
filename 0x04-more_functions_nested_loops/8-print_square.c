dd42082b89e18d862616f26364d465109058a19a
Kolejojoses <kolejojoses@gmail.com>
2022-03-17 20:49:59 +0000

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_square - a function that prints a square
+*@size: notation
+*Return: 0
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 1; a < size; a++)
+		{
+			for (b = 1; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
