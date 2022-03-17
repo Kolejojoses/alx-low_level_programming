e7adca2b522e48fef0d40835a7c0bd9a7aad4e46
Kolejojoses <kolejojoses@gmail.com>
2022-03-17 21:52:29 +0000

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 /**
 *main - Fizz-Buzz
@@ -13,12 +14,12 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
 			printf("Fizz");
 		else if (a % 5 == 0)
 			printf("Buzz");
-		else if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz");
 		else
 			printf("%d", a);
 		if (a != 100)
