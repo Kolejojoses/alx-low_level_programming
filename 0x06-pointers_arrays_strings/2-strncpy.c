2222daa40a526883afa95c2e6075f2e9ed9f9c5b
Kolejojoses <kolejojoses@gmail.com>
2022-03-23 10:51:25 +0000

    3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,11 @@
 #include "main.h"
 
 /**
-*
-*
-*
+*_strncpy - a function that copies a string.
+*@dest: destination
+*@src: source
+*@n: n byte
+*Return: dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
