5f1b1c6db5718a6b7a04899d7a8adcee287ed2f9
Kolejojoses <kolejojoses@gmail.com>
2022-03-24 18:38:05 +0000

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  *print_number -  a function that prints a number
+  *@n: pointer
+  *Return: nothing
+  */
+
+void print_number(int n)
+{
+	int np;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		np = -n;
+	}
+	else
+	{
+		np = n;
+	}
+	if (np / 10)
+	{
+		print_number(np / 10);
+	}
+	_putchar(np % 10 + '0');
+}
