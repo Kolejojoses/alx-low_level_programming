4687fc845ca5e859affdfc7561666fb539dade07
Kolejojoses <kolejojoses@gmail.com>
2022-03-28 15:38:16 +0000

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int sum_1 = 0;
 	int sum_2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum_1 = sum_1 + a[i][i]
-		sum_2 = sum_2 + a[i][size - i]
+		sum_1 = sum_1 + a[i][i];
+		sum_2 = sum_2 + a[i][size - i];
 		printf("%d, %d", sum_1, sum_2);
 	}
 }
