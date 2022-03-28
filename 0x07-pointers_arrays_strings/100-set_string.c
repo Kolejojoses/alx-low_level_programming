80b618d2a8ed89b310213f01a8bedbcb13ec6288
Kolejojoses <kolejojoses@gmail.com>
2022-03-28 19:45:57 +0000

    100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,11 +3,11 @@
 /**
 *set_string - value of a pointer to char
 *@s: string
-*@to: parameter 
+*@to: parameter
 *Return: nothing
 */
 
 void set_string(char **s, char *to)
 {
-	s = to;
+	*s = to;
 }
