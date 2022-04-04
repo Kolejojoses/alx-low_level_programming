688b4bb1581b1a39659029f6da73cdd14a47efd4
Kolejojoses <kolejojoses@gmail.com>
2022-04-04 13:47:40 +0000

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - 2-d array
+ *@width: width of array
+ *@height: height of array
+ *Return: 2-d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+}
