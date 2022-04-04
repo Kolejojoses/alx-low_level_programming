b4f6b29cf5301cbadd66ecbadd4338b4da410f33
Kolejojoses <kolejojoses@gmail.com>
2022-04-04 23:48:16 +0000

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 
+int word_count(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' && s[1 + i] != ' ')
+			count++;
+	}
+	return (count);
+}
+
 /**
  *strtow - splits a string
  *@str: string
@@ -9,4 +22,9 @@
 
 char **strtow(char *str)
 {
+	if (str == NULL || str == "")
+		return (NULL);
+	
+	if (ptr == NULL)
+		return (NULL);
 }
