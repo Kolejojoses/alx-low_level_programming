85f3f715dec17fa585178f9b72d77bf36c4b88fa
Kolejojoses <kolejojoses@gmail.com>
2022-04-04 10:34:35 +0000

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,5 +11,15 @@
 
 char *create_array(unsigned int size, char c)
 {
-	
+	unsigned int i;
+	char *ptr = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+	*(ptr + i) = 0;
+	return (ptr);
 }
