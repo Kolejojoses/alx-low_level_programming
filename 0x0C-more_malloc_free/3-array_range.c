b9d18f5261b82d7cdaf48c0a4fa2eeddb6f5d162
Kolejojoses <kolejojoses@gmail.com>
2022-04-07 15:17:21 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,11 +10,17 @@
 
 int *array_range(int min, int max)
 {
-	int *ptr;
+	int *ptr, i;
 
 	if (min > max)
 		return (NULL);
-
+	ptr = (int *)malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; min <= max; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
+	return (ptr);
 }
