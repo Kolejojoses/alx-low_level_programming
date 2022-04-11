6c0b7f5e03a96c02a643525c9b0d633747ff31b5
Kolejojoses <kolejojoses@gmail.com>
2022-04-11 05:44:06 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - dog to be copied
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog = NULL)
+		return (NULL);
+	return (new_dog);
+}
