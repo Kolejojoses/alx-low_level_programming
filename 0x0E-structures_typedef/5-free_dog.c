955cf75f32e429931fe648adea6e0527415d5cd0
Kolejojoses <kolejojoses@gmail.com>
2022-04-11 06:04:48 -0700

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - free spce
+ * @d: dog
+ * Return: nothing
+ */
+
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
 	if (d->owner != NULL)
 		free(d->owner);
-	if(d->name != NULL)
+	if (d->name != NULL)
 		free(d->name);
 	free(d);
 }
