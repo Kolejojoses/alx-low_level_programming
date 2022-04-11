b6780cc8bec139620e35410420e46c29db34491a
Kolejojoses <kolejojoses@gmail.com>
2022-04-11 07:12:46 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,10 +23,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	j = strlen(owner);
 	copy->name = malloc(sizeof(char) * (i + 1));
 	if (copy->name == NULL)
+	{
+		free(copy);
 		return (NULL);
+	}
 	copy->owner = malloc(sizeof(char) * (j + 1));
 	if (copy->owner == NULL)
+	{
+		free(copy);
+		free(copy->name);
 		return (NULL);
+	}
 	strcpy(copy->name, name);
 	strcpy(copy->owner, owner);
 	copy->age = age;
