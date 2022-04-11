b1506328bad5dd85f7d9e4304f1486669bb95815
Kolejojoses <kolejojoses@gmail.com>
2022-04-11 06:59:09 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * new_dog - dog to be copied
@@ -12,7 +13,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog = NULL)
-		return (NULL);
-	return (new_dog);
+	dog_t *copy;
+	int i, j;
+
+	copy = malloc(sizeof(dog_t));
+	i = strlen(name);
+	j = strlen(owner);
+	copy->name = malloc(sizeof(char) * (i + 1));
+	copy->owner = malloc(sizeof(char) * (j + 1));
+	strcpy(copy->name, name);
+	strcpy(copy->owner, owner);
+	copy->age = age;
+	return (copy);
 }
