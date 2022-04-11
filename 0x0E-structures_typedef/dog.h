bd0feb0c0966cec1e65770bccaeaf42ddd19a1d1
Kolejojoses <kolejojoses@gmail.com>
2022-04-11 04:30:02 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
