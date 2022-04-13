0a9bb83a7842b6e9f825b37fd33eaa03f197c24b
kolejojoses <kolejojoses@gmail.com>
2022-04-13 15:08:14 +0100

    init

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * 
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
