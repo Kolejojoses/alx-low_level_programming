2e54cb3122c05221143170694c574c0ada18ab93
Kolejojoses <kolejojoses@gmail.com>
2022-04-14 07:50:17 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 		else if (i < n - 1 && separator == NULL)
 		{
-			printf("%d ", va_arg(args, int));
+			printf("%d", va_arg(args, int));
 		}
 		else if (i == n - 1)
 		{
